b51fbfcd3b22e11f8bfb7223f32499f023e107ad
Ryan Hinks <4hinkr03@solent.ac.uk>
2019-03-27 11:59:44 +0000

    Increased no. of autobots for a greater population. Increased max speed to help aid testin.g

--- src/ryan/transformers/TransformerConfig.java
+++ src/ryan/transformers/TransformerConfig.java
@@ -1,5 +1,6 @@
 package ryan.transformers;
 
+import prins.simulator.Config;
 import prins.simulator.model.Location;
 
 import java.util.Random;
@@ -8,7 +9,7 @@ public class TransformerConfig {
 
     private static final int RANDOM_SEED = 28;
 
-    public static final int MAX_TRANSFORMERS = 10;
+    public static final int MAX_TRANSFORMERS = 100;
     public static final int MAX_PATH = 30;
     public static  final int MAX_SIZE = 5;
     public static Random RANDOM = new Random(RANDOM_SEED);
@@ -20,5 +21,8 @@ public class TransformerConfig {
         RANDOM = new Random(RANDOM_SEED);
     }
 
+    static {
+        Config.max_simulation_speed = 2;
+    }
 
 }
